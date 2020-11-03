c6cd5eeaef4cfe282ab035c4b7da67d1e5b3565c
Baran Akçakaya <baranakcakaya@hotmail.com>
2020-11-03 11:56:34 +0300

    Update FaceRecognitionWithLBP.py

--- LBP/FaceRecognitionWithLBP.py
+++ LBP/FaceRecognitionWithLBP.py
@@ -17,22 +17,22 @@ if(str(user) not in str(os.listdir('dataset'))):
     os.mkdir('dataset/'+user)
 temp = 0
 
-# while(True):
-#     ret, frame = cap.read()
-#     frame = cv2.flip(frame, 1)
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     faces = face_detector.detectMultiScale(gray, 1.5, 5)
-    
-#     for (x, y, w, h) in faces:
-#         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-#         temp += 1
-#         print(temp)
-#         cv2.imwrite("dataset/"+ user + '/' + str(temp+1) + ".jpg", gray[y:y+h,x:x+w])        
-#         cv2.imshow('frame',frame)        
-#     if cv2.waitKey(1) & 0xFF == ord('Q'):
-#         break
-#     elif(temp > 100):
-#         break
+while(True):
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray, 1.5, 5)
+  
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        temp += 1
+        print(temp)
+        cv2.imwrite("dataset/"+ user + '/' + str(temp+1) + ".jpg", gray[y:y+h,x:x+w])        
+        cv2.imshow('frame',frame)        
+    if cv2.waitKey(1) & 0xFF == ord('Q'):
+        break
+    elif(temp > 100):
+        break
     
 cap.release()
 cv2.destroyAllWindows()
